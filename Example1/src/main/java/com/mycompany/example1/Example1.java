53f7cea45a3751a426a8a3edcc403415f4fe7ba0
ruvindya28 <136114798+ruvindya28@users.noreply.github.com>
2023-07-27 20:56:16 +0530

    practical 1-2023-074-27(question 2)

---
+++ Example1/src/main/java/com/mycompany/example1/Example1.java
@@ -0,0 +1,10 @@
+
+package com.mycompany.example1;
+
+public class Example1 {
+
+    public static void main(String[] args) {
+        System.out.println("My name is sachini");
+         System.out.println("My degree program is software engineering");
+    }
+}
