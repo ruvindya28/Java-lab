f46005bfbfea998fedb4b2b3e03f6f7e0e0d2a03
ruvindya28 <136114798+ruvindya28@users.noreply.github.com>
2023-07-27 21:09:19 +0530

    practical 1-2023-07-27(question 3)

---
+++ Example3/src/main/java/com/mycompany/example3/Example3.java
@@ -0,0 +1,15 @@
+
+package com.mycompany.example3;
+
+public class Example3 {
+
+    public static void main(String[] args)
+    {
+      int x=0;
+      while(x<=4)
+      {
+          System.out.println("Executing loop "+x);
+          x++;
+      }
+    }
+}
---
+++ Example4/src/main/java/com/mycompany/example4/Example4.java
@@ -0,0 +1,13 @@
+
+package com.mycompany.example4;
+
+public class Example4 {
+
+    public static void main(String[] args)
+    {
+        for(int i=0;i<=4;i++)
+        {
+            System.out.println("Executing loop"+i);
+        }
+    }
+}
