bf6fb840cca90fb3c609b110ec20325676dd6784
ruvindya28 <136114798+ruvindya28@users.noreply.github.com>
2023-07-27 22:08:20 +0530

    practical 1-2023-07-27(question 6)

---
+++ TestEnhanceForLoop/src/main/java/com/mycompany/testenhanceforloop/TestEnhanceForLoop.java
@@ -0,0 +1,28 @@
+
+package com.mycompany.testenhanceforloop;
+
+public class TestEnhanceForLoop {
+
+    public static void main(String[] args)
+    {
+      	 int [] numbers = {10, 20, 30, 40, 50};
+      		for(int x : numbers ){
+        			 System.out.print( x );
+         			System.out.print(",");
+      		}
+	 System.out.print("\n");
+      	String [] names ={"James", "Larry", "Tom", "Lacy"};
+      		for( String name : names ) {
+        		 	System.out.print( name );
+         			System.out.print(",");
+
+    }
+}
+}
+
+//error 
+ //there is a missing semicolon after the array initialization for the names array.
+
+//output
+//10,20,30,40,50,
+//James,Larry,Tom,Lacy,
