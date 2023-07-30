2905dc4937cb5cd998b77d7bef1e13b10d6ca4b8
ruvindya28 <136114798+ruvindya28@users.noreply.github.com>
2023-07-30 10:26:52 +0530

    practical 2-2023-07-30(question 3)

---
+++ Mcq2/src/main/java/com/mycompany/mcq2/Mcq2.java
@@ -0,0 +1,36 @@
+
+package com.mycompany.mcq2;
+
+public class Mcq2 {
+
+    public static void main(String[] args) 
+    {
+     // 1.Real-world objects contain ___ and ___.
+        //answer-data and method
+        
+    //2.A software object's state is stored in ___.
+        //answer-instance variable
+        
+     //3.A software object's behavior is exposed through ___.
+        //answer-methods
+        
+    //4.Hiding internal data from the outside world, and accessing it only through publicly exposed methods is known as data ___.
+        //answer-encapsulation
+        
+    //  5.	A blueprint for a software object is called a ___.
+        //answer-class
+        
+   //   6.	Common behavior can be defined in a ___ and inherited into a ___ using the ___ keyword.
+        //answer-superclass
+        
+   //  7.	A collection of methods with no implementation is called an ___.
+        //answer-interface
+        
+   //  8.	A namespace that organizes classes and interfaces by functionality is called a ___.
+        //answer-package
+        
+   //  9.	The term API stands for ___?
+        //answer-application programming interface
+
+    }
+}
