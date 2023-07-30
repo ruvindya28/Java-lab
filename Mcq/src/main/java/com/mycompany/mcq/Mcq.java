904baa10a08c4aa7539a735af92dbe926a09b0aa
ruvindya28 <136114798+ruvindya28@users.noreply.github.com>
2023-07-30 10:01:12 +0530

    practical 2-2023-07-30(question 2)

---
+++ Mcq/src/main/java/com/mycompany/mcq/Mcq.java
@@ -0,0 +1,30 @@
+
+package com.mycompany.mcq;
+
+public class Mcq {
+
+    public static void main(String[] args)
+    {
+      //1. Which of these keywords is used to refer to member of base class from a sub class?
+ 	//a) upper	b) super	c) this		d) None of the mentioned
+
+      //3.	The modifier which specifies that the member can only be accessed in its own class is
+        //a) public		b) private	c) protected		d) none 
+
+
+     //4.	Which of these is a mechanism for naming and visibility control of a class and its content?
+       //a) Object	     b) Packages              c) Interfaces	         d) None of the Mentioned.
+
+
+     //5.	Which of the following is correct way of importing an entire package ‘pkg’?
+     //a) import pkg.	     b) Import pkg.            c) import pkg.*	         d) Import pkg.*
+
+
+     //6.	Which of these method of class String is used to extract a single character from a String object?
+     //a) CHARAT()	       b) charat()        c) charAt()	         d) CharAt()
+
+     //7.	Which of these method of class String is used to obtain length of String object?
+    //   a) get()	       b) Sizeof()         c) lengthof()	          d) length()
+    
+    }
+}
